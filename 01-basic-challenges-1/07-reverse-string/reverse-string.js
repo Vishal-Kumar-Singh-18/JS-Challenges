07e5caaae7aedbfd9e4c94b9ad0d8cdab0d04a31
Vishal-Kumar-Singh-18 <vishalkr.18singh1999@gmail.com>
2025-12-01 23:25:38 +0530

    Solved reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,16 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    console.log(str[i]);
+    reversedStr += str[i];
+  }
+
+  return reversedStr;
+}
+
+// const reverseString = (str) => str.split('').reverse().join('');
+// Here we need to split first bcz reverse() method in not available direvtly to 'String'
+// Therefore using split converted it to an array
 
 module.exports = reverseString;

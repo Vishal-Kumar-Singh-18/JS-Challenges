bf499ad438ac06dacfc51a27c61a5bfbacb2d245
Vishal-Kumar-Singh-18 <vishalkr.18singh1999@gmail.com>
2025-12-01 23:56:48 +0530

    Solved palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,15 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let reverse = "";
+  const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  for (let i = formattedStr.length - 1; i >= 0; i--) {
+    reverse += formattedStr[i];
+  }
+  if (formattedStr === reverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = isPalindrome;

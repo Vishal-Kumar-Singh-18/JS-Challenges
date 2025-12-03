19245c6187ce1c7251234d0eb5e87abeaf13d42a
Vishal-Kumar-Singh-18 <vishalkr.18singh1999@gmail.com>
2025-12-03 18:01:29 +0530

    Completed basic challenges 01

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,17 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    let newArray = [];
+
+    for(let i=0; i<arr.length; i++) {
+        if (newArray.includes(arr[i]) !== true) {
+            newArray.push(arr[i])
+        }
+    }
+
+    return newArray;
+}
+
+// function removeDuplicates(arr) {
+//   return Array.from(new Set(arr));
+// }
 
 module.exports = removeDuplicates;

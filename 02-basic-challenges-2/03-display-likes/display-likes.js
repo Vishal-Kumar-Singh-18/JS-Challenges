8d4e8a84e878172bae48a20e8700d6d8ededf93b
Vishal-Kumar-Singh-18 <vishalkr.18singh1999@gmail.com>
2025-12-04 14:04:57 +0530

    Completed Display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,18 @@
-function displayLikes() {}
-
+function displayLikes(arr) {
+  let count = 0;
+  if (arr.length === 0) {
+    return "no one likes this";
+  } else if (arr.length >= 4) {
+    for (let i = 2; i < arr.length; i++) {
+      count++;
+    }
+    return `${arr[0]}, ${arr[1]} and ${count} others like this`;
+  } else if (arr.length === 1) {
+    return `${arr[0]} likes this`;
+  } else if (arr.length === 2) {
+    return `${arr[0]} and ${arr[1]} like this`;
+  } else if (arr.length === 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  }
+}
 module.exports = displayLikes;

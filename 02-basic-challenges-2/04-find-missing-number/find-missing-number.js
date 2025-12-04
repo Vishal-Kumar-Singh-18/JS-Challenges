c104f1db1aeb55d1c43c3bad7ac009843d685969
Vishal-Kumar-Singh-18 <vishalkr.18singh1999@gmail.com>
2025-12-04 23:54:14 +0530

    Solved 3 more problems

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,16 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    if (arr.length === 0) return 1
+
+    let maxNum = arr.length + 1;
+    let maxSum = (maxNum*(maxNum+1))/2;
+    let sum = 0;
+
+    for (const n of arr) {
+        sum += n
+    }
+
+    return maxSum - sum;
+
+}
 
 module.exports = findMissingNumber;
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,18 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    // Object 
+    const charCount = {}
+
+    for(let i=0; i<str.length; i++) {
+        const char = str[i]
+
+        if (charCount[char]) {
+            return false
+        }
+
+        charCount[char] = true;
+    }
+
+    return true;
+}
 
 module.exports = areAllCharactersUnique;

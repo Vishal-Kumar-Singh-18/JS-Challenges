6226e4c89ec19741212d38f5b15455e2a4303bf8
Vishal-Kumar-Singh-18 <vishalkr.18singh1999@gmail.com>
2025-12-05 20:30:41 +0530

    Completed firsr non repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,23 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    const charCount = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i]; 
+        if (charCount[char] === undefined) {
+            charCount[char] = 1;
+        } else {
+            charCount[char] = charCount[char] + 1;
+        }
+    }
+
+    for (const char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
+
+}
 
 module.exports = findFirstNonRepeatingCharacter;
